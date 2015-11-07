58cc9116f4cfc1940743ac2b2b1f29320f20be2b
Ben Olive <sionide21@gmail.com>
2015-11-07 10:06:09 -0500

    Add home page and remove unwanted feature specs

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
+
+  root 'root#index'
 end
--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -9,22 +9,22 @@ feature 'Sign in', :omniauth do
   #   And I am not signed in
   #   When I sign in
   #   Then I see a success message
-  scenario "user can sign in with valid account" do
-    signin
-    expect(page).to have_content("Sign out")
-  end
+  # scenario "user can sign in with valid account" do
+  #   signin
+  #   expect(page).to have_content("Sign out")
+  # end
 
   # Scenario: User cannot sign in with invalid account
   #   Given I have no account
   #   And I am not signed in
   #   When I sign in
   #   Then I see an authentication error message
-  scenario 'user cannot sign in with invalid account' do
-    OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
-    visit root_path
-    expect(page).to have_content("Sign in")
-    click_link "Sign in"
-    expect(page).to have_content('Authentication error')
-  end
+  # scenario 'user cannot sign in with invalid account' do
+  #   OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
+  #   visit root_path
+  #   expect(page).to have_content("Sign in")
+  #   click_link "Sign in"
+  #   expect(page).to have_content('Authentication error')
+  # end
 
 end
